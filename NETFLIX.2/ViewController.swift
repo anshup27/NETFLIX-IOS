2b67c830b6620dc66ae9e313ea6324baa70c3c48
anshup27 <ansuprasad001@gmail.com>
2023-12-25 15:18:12 +0530

    Initial Commit

---
+++ NETFLIX.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NETFLIX.2
+//
+//  Created by Anshu Prasad on 25/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
